4ca8fa9ecd3271c7f389b9331cef5e9e372a8193
lumine <463305152@qq.com>
2024-10-26 14:09:43 +0800

    only server

--- src/main/java/io/github/cpearl0/gtmmodify/GTMModify.java
+++ src/main/java/io/github/cpearl0/gtmmodify/GTMModify.java
@@ -2,9 +2,11 @@ package io.github.cpearl0.gtmmodify;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 import org.slf4j.Logger;
 
 @Mod(GTMModify.MODID)
@@ -15,6 +17,7 @@ public class GTMModify {
     public GTMModify(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        context.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+        context.registerDisplayTest(new IExtensionPoint.DisplayTest(IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 }
